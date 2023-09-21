9954d5cc1bf7384fdf8eb611625a1b250672dfe1
smashcoder2003 <106431623+smashcoder2003@users.noreply.github.com>
2023-09-21 10:36:41 +0530

    Fix issue with ES6 syntax

--- api/src/properties.js
+++ api/src/properties.js
@@ -2,12 +2,19 @@ let properties = {
     java : {
         compiled: true,
         normal_extension: 'java',
-        compiled_extension: 'class'
+        compiled_extension: 'class',
+        version: 'Jre11',
     },
     python: {
         compiled: false,
-        normal_extension: 'py'
+        normal_extension: 'py',
+        version:  '3.10',
+    },
+    r: {
+        compiled: false,
+        normal_extension: 'r',
+        version: '3.6+'
     }
 }
 
-module.exports = properties
+module.exports = properties;
