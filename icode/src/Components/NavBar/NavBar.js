9c28437280824b165283a1a2d286102c60896c8e
BhargavBunty <bhargavbunty@BhargavBuntys-Laptop.local>
2023-10-19 21:30:19 +0530

    Add Hover Functionality

--- icode/src/Components/NavBar/NavBar.js
+++ icode/src/Components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar({handleSignUp, handleAssignments}) {
     }
     return (
       <>
-          <div className="border-b border-gray-200 dark:border-gray-700">
+          <div className="border-b border-gray-200 dark:border-gray-700 sticky top-0 bg-white z-[5]">
               <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
                   <li className="mr-2">
 
@@ -30,15 +30,13 @@ export default function NavBar({handleSignUp, handleAssignments}) {
                   <div onClick={clickedAssignments}>
                       <li className="mr-2">
                           <a href="#" className="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg group hover:text-blue-600 hover:border-blue-600">
-                              <svg className="w-4 h-4 mr-2 text-gray-400 fill-current group-hover:text-blue-600 dark:group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                  <path d="M5 11.424V1a1 1 0 1 0-2 0v10.424a3.228 3.228 0 0 0 0 6.152V19a1 1 0 1 0 2 0v-1.424a3.228 3.228 0 0 0 0-6.152ZM19.25 14.5A3.243 3.243 0 0 0 17 11.424V1a1 1 0 0 0-2 0v10.424a3.227 3.227 0 0 0 0 6.152V19a1 1 0 1 0 2 0v-1.424a3.243 3.243 0 0 0 2.25-3.076Zm-6-9A3.243 3.243 0 0 0 11 2.424V1a1 1 0 0 0-2 0v1.424a3.228 3.228 0 0 0 0 6.152V19a1 1 0 1 0 2 0V8.576A3.243 3.243 0 0 0 13.25 5.5Z"/>
+                              <svg className="w-4 h-4 mr-2 text-gray-400 fill-current group-hover:text-blue-600 dark:group-hover:text-blue-600 hover:text-blue-600 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                  <path d="M5 22h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2h-2a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1H5c-1.103 0-2 .897-2 2v15c0 1.103.897 2 2 2zM5 5h2v2h10V5h2v15H5V5z"></path><path d="m11 13.586-1.793-1.793-1.414 1.414L11 16.414l5.207-5.207-1.414-1.414z"/>
                               </svg>
                               Assignments
                           </a>
                       </li>
                   </div>
-
-
 
                   <div onClick={clickedSignUp}>
                       <li className="mr-2">
