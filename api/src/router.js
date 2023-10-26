df39a61e56124e8245844c19c8dfd8fc20f18604
BhargavBunty <bhargavbunty@059gmail.com>
2023-10-26 17:48:47 +0530

    add /login and /logged_in endpoints

--- api/src/router.js
+++ api/src/router.js
@@ -5,7 +5,9 @@ const Job = require('./Job');
 const Logger = require('logplease');
 const logger = Logger.create('Router');
 const Job2 = require('./Job2');
-
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+require('dotenv').config();
 
 router.use((req, res, next) => {
     if (!['GET', 'OPTIONS', 'POST'].includes(req.method)) {
@@ -16,15 +18,75 @@ router.use((req, res, next) => {
     return next();
 });
 
-router.get('/codeeditor',(req, res) =>{
-    res.status(200).sendFile(path.join(__dirname, 'public', 'codeground.html'))
+router.get('/logged_in', async (req, res) => {
+    try {
+        const token = req.cookies.token;
+        logger.log('Extracted Cookie');
+        if (!token) res.json({ loggedIn: false });
+        const { user } = jwt.verify(token, process.env.TOKEN_SECRET);
+        logger.log("verified Cookie");
+        const newToken = jwt.sign({ user }, process.env.TOKEN_SECRET, { expiresIn: 3600000 });
+        logger.log("Generated NewCookie");
+        res.cookie('token', newToken, { maxAge: 3600000, httpOnly: true });
+        logger.log("Sent NewCookie");
+        // request should send the userDetails as it's body
+        res.status(200).json({ signedIn: true, user: user.userName });
+        logger.log("Sent login Response");
+    } catch (err) {
+        console.log(err);
+        res.status(200).json({ signedIn: false, message: err.message });
+    }
 });
+
+
+router.post('/login', async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+
+        const result = await axios.post('http://localhost:2000/database/validate_user', {
+            user: userName,
+            password: password
+        }, { headers: { 'Content-Type': 'Application/json' } });
+
+
+        console.log(result);
+        logger.log(result);
+        if (result.data["authorized"] !== true) res.status(400).json({ message: "Invalid RollNo or Password" } );
+
+
+        const user  = { userName: userName };
+        logger.log(`USER: ${user}`);
+
+
+        logger.log(`THIS IS THE TOKEN SECRET FOR TESTING: ${process.env.TOKEN_SECRET}`);
+        logger.log("Creating a token for the user");
+        const newToken = jwt.sign({ user }, process.env.TOKEN_SECRET, { expiresIn: 36000});
+        logger.log("Created a token for the user");
+
+
+        res.cookie('token', newToken, { maxAge: 36000, httpOnly: true });
+        logger.log("sent Cookies");
+        res.status(200).json({ message: "You have been signed in!! ", loggedIn: true, user:  userName });
+        logger.log("sent response");
+
+    } catch (err) {
+        console.log(err);
+        res.status(200).json({ loggedIn: false, message: err.message });
+    }
+});
+
+
+router.get('/codeeditor',(req, res) =>{
+    res.status(200).sendFile(path.join(__dirname, 'public', 'build/index.html'))
+});
+
 
 router.get('/', (req, res) => {
     res.status(200).send({
         response: 'Response ok'
     });
 });
+
 
 router.get('/runtimes', (req, res) => {
     res.status(200).send({
