01c1866bbcdb3b2a4077703626ee7b20cadb37b7
BhargavBunty <bhargavbunty@BhargavBuntys-Laptop.local>
2023-10-26 17:43:20 +0530

    Add database router and cookie parser

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const PORT = process.env.PORT || 2000;
 const rateLimit = require("express-rate-limit");
 const cors = require('cors');
+const databaseRouter = require("./databaseRoutes/databaseRouter");
+const cookieParser = require('cookie-parser');
+
 app.use(cors());
 
 const limiter = rateLimit({
@@ -15,11 +18,15 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use("/api", router);
+app.use("/database", databaseRouter);
+
 
 app.listen(PORT, () => {
     console.log(`The server is live at http://localhost:${PORT}`);
